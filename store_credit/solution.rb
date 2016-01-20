ddf37d9c00550a5af6de423469f785f846199f6b
Stephen Grimes <stgrimes@gmail.com>
2016-01-20 01:08:45 -0500

    revised solution

--- store_credit/solution.rb
+++ store_credit/solution.rb
@@ -2,21 +2,23 @@
 
 require 'set'
 
+credit = 0
 File.open('in').readlines.each_with_index do |line, i|
   next if i == 0
   next if i % 3 == 2
   if i % 3 == 1
     credit = line.strip.to_i
-    puts "Credit #{credit}"
-  else
+  elsif i % 3 == 0
     prices = line.split(/\s/)
     case_num = i / 3
     s = Set.new
-    prices.each { |p| s.add(p) }
+    prices.each { |p| s.add(p.to_i) }
+    hash = Hash[prices.map.with_index.to_a]
     prices.each_with_index do |p,index|
       a = credit.to_i - p.to_i
-      next unless s.include?(a.to_s)
-      puts "Case ##{case_num}: #{index} #{prices.index((credit-p.to_i).to_s)}"
+      next unless s.include?(a)
+      puts "Case ##{case_num}: #{index+1} #{hash[a.to_s]+1}"
+      break
     end
   end
 end
